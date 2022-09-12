c47c8fde1d1895612e8c85b65da56e1cacbb5124
Hriday31 <hriday311203@gmail.com>
2022-09-12 14:17:06 +0530

    some changes

---
+++ index.js
@@ -0,0 +1,13 @@
+ 	
+   	 var  nav = document.getElementByclass('bh');
+      
+      window.onscroll = function(){
+
+      	if (window.pageYOffset >100) {
+
+      		nav.style.background = "#007bff";
+      	}
+      	else{
+      		nav.style.background = "transparent";
+      	}
+      }
